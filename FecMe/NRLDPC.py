a524f4de9b6753eb6e6514640621b7f01e8b18d8
Salvatore Cardamone <sav.cardamone@gmail.com>
2020-01-08 19:43:36 +0000

    Segmentation appears to work

--- FecMe/NRLDPC.py
+++ FecMe/NRLDPC.py
@@ -1,9 +1,10 @@
-from numpy import ceil
+from math import inf
+from numpy import array, zeros, ceil, nonzero, min, where, concatenate
+from numpy.random import choice
 
-from FecMe.CRC import polynomials, checksum
-from FecMe.LDPC import LDPC
+from CRC import polynomials, checksum
 
-class NRLDPC(LDPC):
+class NRLDPC():
     """New Radio LDPC Encode/Decode.
     """
     
@@ -80,7 +81,7 @@ class NRLDPC(LDPC):
         """Return the number of codeblocks the transport block is to be segmented into.
         """
         if self.B <= self.Kcb:
-            return 0
+            return 1
         else:
             return int(ceil(self.B / (self.Kcb - self.L)))
 
@@ -94,7 +95,7 @@ class NRLDPC(LDPC):
     def Kprime(self):
         """Return the number of bits in each codeblock.
         """
-        return self.Bprime / self.C
+        return self.Bprime // self.C
 
     @property
     def Kb(self):
@@ -111,7 +112,41 @@ class NRLDPC(LDPC):
                 return 8
             else:
                 return 6
+
+    @property
+    def Zc(self):
+        """Return the lifting size for the base graph.
+        """
+        # Table 5.3.2-1 from 38.212
+        lifting_sizes = array(
+            [[2,4,8,16,32,64,128,256],
+             [3,6,12,24,48,96,192,384],
+             [5,10,20,40,80,160,320,0],
+             [7,14,28,56,112,224,0,0],
+             [9,18,36,72,144,288,0,0],
+             [11,22,44,88,176,352,0,0],
+             [13,26,52,104,208,0,0,0],
+             [15,30,60,120,240,0,0,0]], dtype=int)
+
+        # Pull out the indices whose entries don't satisfy the inequality Kb.Zc >= K'
+        sub_threshold_indices = lifting_sizes < self.Kprime / self.Kb
+        # Set subthreshold elements to zero
+        lifting_sizes[sub_threshold_indices] = 0
+        # Now pull out the smallest non-zero element from the lifting sizes and that's our
+        # desired lifting size
+        lifting_set, z = where(lifting_sizes == min(lifting_sizes[nonzero(lifting_sizes)]))
+        
+        return int(lifting_sizes[lifting_set,z])
     
+    @property
+    def K(self):
+        """Return the number of bits the encoded codeblock will comprise.
+        """
+        if self.BGN == 1:
+            return 22 * self.Zc
+        else:
+            return 10 * self.Zc
+        
 
     ### ====================================================================================
     ###                                     Methods
@@ -128,9 +163,8 @@ class NRLDPC(LDPC):
             are to be encoded separately.
             See 38.212 Section 5.2.2. for details.
             """
-    
             # Initialise our segmented codeblocks with zeros
-            c = zeros((self.C,self.Kprime), dtype=int)
+            c = zeros((self.C,self.K), dtype=int)
 
             # Counter we manually increment when filling message bits
             s = 0
@@ -146,16 +180,26 @@ class NRLDPC(LDPC):
                 if self.C > 1:
                     p = CRC.checksum(c[r,self.Kprime - self.L], polynomial='CRC24B', checksum_fill=0)
                     # Append the checksum bits to the message bits in the codeblock
-                    for k in range((self.Kprime - self.L):self.Kprime):
+                    for k in range((self.Kprime-self.L), self.Kprime):
                         c[r,k] = p[k + self.L - self.Kprime]
 
                 # Pad the remaining bits in the codeblock with filler bits
-                for k in range(self.Kprime:self.K):
+                for k in range(self.Kprime, self.K):
                     c[r,k] = -1
 
             return c
 
         def parity(self, c):
+            pass
+
+        def rate(self, d):
+            def select(self):
+                pass
+            def interleave(self):
+                pass
+            pass
+
+        def concatenation(self, f):
             pass
         
         # Make sure we're not trying to encode something that doesn't belong
@@ -163,8 +207,23 @@ class NRLDPC(LDPC):
             raise ValueError("Encoder has been parameterised for {0} bits, but {1} have been passed".format(self.A, len(a)))
 
         # QQ: Not sure if this is the right polynomial. Can't find it in the spec...
-        b = concatenate(a, CRC.checksum(a, polynomial='CRC24A', checksum_fill=0))
+        b = concatenate((a, checksum(a, polynomial='CRC24A', checksum_fill=0)))
         # Transport block segmentation
         c = segmentation(self, b)
         # Generate parity bits for each codeblock
         d = parity(self, c)
+        # Rate matching
+        f = rate(self, d)
+        # Codeblock concatenation
+        g = concatenation(self, f)
+
+        return g
+
+
+if __name__ == "__main__":
+
+    A = 63
+    ldpc = NRLDPC(A)
+    a = choice([0,1], size=(A,))
+
+    ldpc.encode(a)
